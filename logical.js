a2f9c6fcbd25d3568d4dd73525b9059a50f2f135
himani79737 <rodhiyalhimani@gmail.com>
2025-10-24 22:28:14 +0530

    Add files via upload

---
+++ logical.js
@@ -0,0 +1,8 @@
+// find the age of the person lies between 10 and 20
+let age = 15;
+if (age >= 10 && age <= 20) {
+    console.log("The age lies between 10 and 20");
+}
+else {
+    console.log("age is invalid ");
+    }
